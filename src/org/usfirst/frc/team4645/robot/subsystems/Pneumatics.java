839697e845b26f636f0efc903f5baea55d861f6d
ktolentino <karen192727@gmail.com>
2018-01-18 17:40:10 -0600

    Fixed up Command Based Pneumatics

---
+++ src/org/usfirst/frc/team4645/robot/subsystems/Pneumatics.java
@@ -0,0 +1,41 @@
+package org.usfirst.frc.team4645.robot.subsystems;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+import org.usfirst.frc.team4645.robot.*;
+import org.usfirst.frc.team4645.robot.commands.PneumaticsCommandIn;
+/**
+ *
+ */
+public class Pneumatics extends Subsystem {
+
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+	//Creates solenoid object
+	
+		DoubleSolenoid solenoid = new DoubleSolenoid(RobotMap.leftPistonIn,RobotMap.leftPistonOut);
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+    
+    public void out() {
+    	solenoid.set(DoubleSolenoid.Value.kForward);
+    	SmartDashboard.putString("Pnuematics","Out");
+    }
+    
+    public void in() 
+    {
+    	solenoid.set(DoubleSolenoid.Value.kReverse);
+    	SmartDashboard.putString("Pnuematics","In");
+    }
+    
+    public void rest() {
+    	solenoid.set(DoubleSolenoid.Value.kOff);
+    }
+
+}
+
