579ed66f8bc1536f5d0277eb1bad09a02d4fcad8
alexa1411 <31996606+alexa1411@users.noreply.github.com>
2018-04-10 20:38:11 -0500

    Update RobotMap.java

--- src/org/usfirst/frc/team4645/robot/RobotMap.java
+++ src/org/usfirst/frc/team4645/robot/RobotMap.java
@@ -1,3 +1,4 @@
+
 /*----------------------------------------------------------------------------*/
 /* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
@@ -6,51 +7,37 @@
 /*----------------------------------------------------------------------------*/
 
 package org.usfirst.frc.team4645.robot;
-
 /**
- * The RobotMap is a mapping from the ports sensors and actuators are wired into
- * to a variable name. This provides flexibility changing wiring, makes checking
- * the wiring easier and significantly reduces the number of magic numbers
- * floating around.
- */
-public class RobotMap 
+* The RobotMap is a mapping from the ports sensors and actuators are wired into
+* to a variable name. This provides flexibility changing wiring, makes checking
+* the wiring easier and significantly reduces the number of magic numbers
+* floating around.
+*/
+
+public class RobotMap
 {
 
 //pneumatics
-	//public static int leftPistonIn = 1;
-	//public static int leftPistonOut = 2;
+	public static int leftPistonIn = 1;
+	public static int leftPistonOut = 2;
 
 //tank drive motors
-	public static int left1 = 14; //14
-	public static int left2 = 15; //15
-	public static int left3 = 16; //16
-	public static int right1 = 11;//11
-	public static int right2 = 12;//12
-	public static int right3 = 13;//13
-	
-	
-//intake motors
-	public static int intake1 = 1;//1	
-	public static int intake2 = 2;//	
-	
+	public static int left1 = 6; //14
+	public static int left2 = 5; //15
+	public static int left3 = 4; //16
+	public static int right1 = 11;
+	public static int right2 = 12;
+	public static int right3 = 13;
 
-//lift motor
-	public static int lift = 10; 
-	
-//lift motor
-	public static int climb1 = 3; 	
-	public static int climb2 = 4; 
-	
+//intake motors
+	public static int intake1 = 1;	
+	public static int intake2 = 2;	
+
+//climb motors
+	public static int climb1 = 3; //was 3
 
 //Analog input
-	public static int ultrasonicChannel1 = 1;
-	public static int ultrasonicChannel2 = 2;
-
-	
-	
-	
-	
-	
-	
-
+	public static int ultrasonicChannel1 = 0;
+	public static int ultrasonicChannel2 = 1;
 }
+
