e1744d3729f78da9c811d58772335fb34444d6cc
alexa1411 <31996606+alexa1411@users.noreply.github.com>
2018-04-10 20:28:46 -0500

    Update PneumaticsCommandIn.java

--- src/org/usfirst/frc/team4645/robot/commands/PneumaticsCommandIn.java
+++ src/org/usfirst/frc/team4645/robot/commands/PneumaticsCommandIn.java
@@ -1,48 +1,46 @@
+
 package org.usfirst.frc.team4645.robot.commands;
-
 import org.usfirst.frc.team4645.robot.Robot;
-
 import edu.wpi.first.wpilibj.command.Command;
 
-/**
- *
- */
-public class PneumaticsCommandIn extends Command 
+public class PneumaticsCommandIn extends Command
 {
-
-    public PneumaticsCommandIn() 
+    public PneumaticsCommandIn()
     {
-    	   //	requires(Robot.pneumaticsSubsystem);
+        requires(Robot.pneumaticsSubsystem);
     }
 
     // Called just before this Command runs the first time
-    protected void initialize() 
+    protected void initialize()
     {
+
     }
 
     // Called repeatedly when this Command is scheduled to run
-    protected void execute() 
+    protected void execute()
     {
-    	//	Robot.pneumaticsSubsystem.in();
-    	
+
+        Robot.pneumaticsSubsystem.in();
     }
 
     // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() 
+    protected boolean isFinished()
     {
         return false;
     }
 
     // Called once after isFinished returns true
-    protected void end() 
+    protected void end()
     {
-    	
+
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
-    protected void interrupted() 
+    protected void interrupted()
     {
-    		//Robot.pneumaticsSubsystem.out();
+        Robot.pneumaticsSubsystem.out();
     }
+
+
 }
