64edd7aeea547ef293dbcff1751c6ea563f0fead
alexa1411 <31996606+alexa1411@users.noreply.github.com>
2018-04-10 20:12:42 -0500

    Update DriveCommand.java

--- src/org/usfirst/frc/team4645/robot/commands/DriveCommand.java
+++ src/org/usfirst/frc/team4645/robot/commands/DriveCommand.java
@@ -1,44 +1,39 @@
+
 package org.usfirst.frc.team4645.robot.commands;
-import org.usfirst.frc.team4645.robot.OI;
 import org.usfirst.frc.team4645.robot.Robot;
 import edu.wpi.first.wpilibj.command.Command;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-
 
 public class DriveCommand extends Command
 {	
+   public DriveCommand()
+   {
+   		requires(Robot.tankDriveSubsystem);
+   }
+   
+   protected void initialize()
+   {
+   		Robot.tankDriveSubsystem.init();
+   }
+   
+   protected void execute()
+   {	
+   		Robot.tankDriveSubsystem.driveWithJoystick();
+   }
+   
+   protected boolean isFinished()
+   {
+   		return false;
+   }
 
-    public DriveCommand() 
-    {
-    		requires(Robot.tankDriveSubsystem);
-    }
+   protected void end()
+   {
+   
+   }
+   
+   protected void interrupted()
+   {
+   		Robot.tankDriveSubsystem.stop();
+   }
 
-    protected void initialize() 
-    {
-    		Robot.tankDriveSubsystem.init();
-    		SmartDashboard.putString("tank drive", "init");
-    }
 
-    protected void execute() 
-    {	
-    		Robot.tankDriveSubsystem.driveWithJoystick();
-    		SmartDashboard.putNumber("joystick1 throttle", OI.joystick1.getThrottle());
-    		SmartDashboard.putString("tank drive", "exec");
-    	
-    }
-
-    protected boolean isFinished() 
-    {
-    		return false;
-    }
-
-    protected void end() 
-    {
-    	
-    }
-
-    protected void interrupted() 
-    {
-    		Robot.tankDriveSubsystem.stop();	
-    }
 }
